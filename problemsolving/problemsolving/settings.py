e7209589634e16d09e8dfdf9837809871d6eca7a
Mahdi Jalali <jalali.m.mahdi@gmail.com>
2020-05-09 19:25:23 +0430

    configing statistic folder in settings.py, adding skeleton css framework + adding a some html files for begining

--- problemsolving/problemsolving/settings.py
+++ problemsolving/problemsolving/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#to make static files work as expected in Windows
+STATICFILES_DIRS=(os.path.join('static'),)
