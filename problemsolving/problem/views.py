5fb107ddadd06bc02c3605381380d13068db33f6
Mahdi Jalali <jalali.m.mahdi@gmail.com>
2020-05-09 18:45:57 +0430

    adding a view and setting the configs for the view, this view renders an html file

--- problemsolving/problem/views.py
+++ problemsolving/problem/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def view_problemlist(request):
+    dataForHtml={
+    'title':"this is the first page"
+    }
+    return render(request,'index.html',dataForHtml)
