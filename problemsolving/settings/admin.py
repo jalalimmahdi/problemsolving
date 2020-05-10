67b380e98d84af0ba6acc32c35dd0c6945bee907
Mahdi Jalali <jalali.m.mahdi@gmail.com>
2020-05-10 20:27:46 +0430

    adding db to the git as I entered some meaning ful information on the DB, and adding a metadaN model to the settings. here we finish the base settings of the software

--- problemsolving/settings/admin.py
+++ problemsolving/settings/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 # Register your models here.
 
-from .models import metadata1
+#first we import models that we want to have admin panel for them
+from .models import metadata1,metadataN
 
+#a form for managing data of metadata1
+#we use metadata1 for configs that will refere by FK of other tables
 class metadata1Admin(admin.ModelAdmin):
-    list_display=('title','description','type')
+    list_display=['title','type','description']
     list_filter=['type']
+    search_fields=['title']
 
 admin.site.register(metadata1,metadata1Admin)
+#end of meatadata1
+
+#a form for managing data of metadataN
+#we use metadataN for configs that will refere by FK of other tables
+class metadataNAdmin(admin.ModelAdmin):
+    list_display=['title','type','description']
+    list_filter=['type']
+    search_fields=['title']
+
+admin.site.register(metadataN,metadataNAdmin)
+#end of meatadataN
--- problemsolving/settings/models.py
+++ problemsolving/settings/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class metadata1(models.Model):
     title=models.CharField(max_length=255)
-    description=models.TextField()
     types_list=(
         ('pt','broblem type'),
         ('ft','finding type'),
@@ -15,6 +14,28 @@ class metadata1(models.Model):
         choices=types_list,
         default="pt"
     )
+    description=models.TextField(blank=True)
+
+    def __str__(self):
+        return self.type + " - " + self.title
+
+
+class metadataN(models.Model):
+    title=models.CharField(max_length=255)
+    types_list=(
+        ('gr','subject group'),
+        ('pl','place'),
+        ('un','company unit'),
+        ('pr','process'),
+        ('go','goal')
+    )
+    type=models.CharField(
+        max_length=2,
+        choices=types_list,
+        default="gr"
+    )
+    description=models.TextField(blank=True)
+    json=models.TextField(blank=True)
 
     def __str__(self):
         return self.type + " - " + self.title
